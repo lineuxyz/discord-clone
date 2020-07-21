d5ff81247d664676b278d5a915da3740b16c369b
Lineu Pastorelli <lineualberto@gmail.com>
2020-07-21 10:24:23 -0300

    add channel list index

---
+++ src/components/ChannelList/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { Container, Category, AddCategoryIcon } from './styles';
+
+import ChannelButton from '../ChannelButton';
+
+const ChannelList: React.FC = () => {
+  return (
+    <Container>
+      <Category>
+        <span>Canais de texto</span>
+        <AddCategoryIcon />
+      </Category>
+
+      <ChannelButton channelName="chat-live" />
+      <ChannelButton channelName="trabalho" />
+      <ChannelButton channelName="lolzinho" />
+      <ChannelButton channelName="valorant" />
+      <ChannelButton channelName="csgo" />
+
+    </Container>
+  )
+}
+
+export default ChannelList;
