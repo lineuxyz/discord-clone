a27d9b398ae2a41a1d9332eb910df41211d6fced
Lineu Pastorelli <lineualberto@gmail.com>
2020-07-21 10:20:13 -0300

    add the dependencies files and main files to start the app

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const { app, BrowserWindow } = require ('electron');
 
+const customTitlebar = require('custom-electron-titlebar');
+
 function createWindow() {
-  const win = new BrowserWindow({ width: 800, height: 600 });
+  const win = new BrowserWindow({ width: 800, height: 600, frame: false, transparent: true, });
   win.loadURL("http://localhost:3000");
 }
+
+new customTitlebar.Titlebar({
+  backgroundColor: customTitlebar.Color.fromHex('#FFFFFF')
+});
 
 app.on("ready", createWindow);
